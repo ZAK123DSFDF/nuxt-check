87d8d745da463c37a4d12fa7af1ec2212e87e820
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-12-15 07:53:35 +0300

    edited

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,35 @@
 <script setup lang="ts">
-const { jokeCache, updateCache, resetCacheIfExpired } = useJokeCache()
+// const { jokeCache, updateCache, resetCacheIfExpired } = useJokeCache()
 const delay = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const { data, status, error, execute } = await useAsyncData(
   "randomJokes",
   async () => {
-    resetCacheIfExpired()
-    if (!jokeCache.value.data) {
-      await delay(3000)
-      return $fetch("https://api.chucknorris.io/jokes/random")
-    } else {
-      return jokeCache.value.data
-    }
+    return $fetch("/api/joke")
   },
   { immediate: false }
 )
 
 onBeforeMount(() => {
-  resetCacheIfExpired()
   execute()
 })
-watch(data, (newData: any) => {
-  if (newData) {
-    updateCache(newData?.value, new Date().toISOString())
-  }
-})
+// watch(data, (newData: any) => {
+//   if (newData) {
+//     updateCache(newData?.value, new Date().toISOString())
+//   }
+// })
 </script>
 
 <template>
   <div v-if="status === 'pending'">Loading...</div>
   <div v-else-if="status === 'error'">Error: {{ error?.message }}</div>
   <div v-else-if="data">
-    <div>{{ jokeCache?.data }}</div>
+    <div>{{ data }}</div>
   </div>
   <div v-else>
     <div>Loading...</div>
   </div>
 
   <NuxtLink to="welcome">welcome</NuxtLink>
-
-  <div>{{ jokeCache?.data }}</div>
+  <div>{{ data }}</div>
 </template>
